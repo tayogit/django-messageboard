b4f47fe65ca3a964a87c20c50efb9d377ba782fe
Adesiyan <tayonetu80@gmail.com>
2018-11-07 13:14:47 -0800

    modified pipfile and installed gunicorn for messageboard

--- m_board_project/settings.py
+++ m_board_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '42=0(kh*s3c_kh9d+4d&i&e=_v+sjtpt!1_bqkr!7q(*u3i%69'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
